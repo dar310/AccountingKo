a61390b54a9b887c8eb618ac3883156f5fe38998
darren <macbook@darren.local>
2025-06-17 18:04:35 +0800

    A template and No functionality as of now

---
+++ app/src/main/java/com/css152l_am5_g8/accountingko/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.css152l_am5_g8.accountingko.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/css152l_am5_g8/accountingko/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.css152l_am5_g8.accountingko.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
